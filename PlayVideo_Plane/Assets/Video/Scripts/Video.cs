3aafeff45156b0276d7be1ff0fb14d157f2b7425
Truong Phu Phat Felix (Zyo) <truongphuphat98@yahoo.fr>
2018-10-23 15:40:39 +0200

    Add PlayVideo_Plane Project

---
+++ PlayVideo_Plane/Assets/Video/Scripts/Video.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Video : MonoBehaviour
+{
+    public MovieTexture movie;
+    private AudioSource audio;
+
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+    }
+
+    void OnMouseDown()
+    {
+        movie.Stop();
+    }
+}
